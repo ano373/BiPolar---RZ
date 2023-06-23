a9f0847e4ff588619162d136f05dcfe2f56f6388
ano373 <96596021+ano373@users.noreply.github.com>
2023-06-24 01:07:17 +0300

    added decoding

--- main.m
+++ main.m
@@ -8,10 +8,29 @@ duration = 2;
 step = 0.01;
 [y,time] = generateHalfDutySignal(ami, duration, step);
 
-
+tiledlayout(2,1)
+nexttile
 plot(time, y);
 xlabel('Time');
 ylabel('AMI Signal');
 title('AMI Encoding');
 axis([0, length(seq)*duration, -1.1, 1.1]);
 grid on;
+
+NoOfSamples = 1/step;
+disp(NoOfSamples)
+w = abs(y);
+
+InputSequnce = DecodeAMI(w, NoOfSamples)
+
+
+
+% Step 5: Display the decoded sequence
+nexttile
+time = 0:length(InputSequnce)-1;  % Generate time axis based on the length of the sequence
+plot(time, InputSequnce);
+xlabel('Time');
+ylabel('Amplitude');
+title('Decoded Signal');
+ylim([-1.5, 1.5])
+grid on
